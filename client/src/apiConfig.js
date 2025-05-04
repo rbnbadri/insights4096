4fce87347b821808abe6458e578ee98177b786f8
rbnbadri <rbn.badri@gmail.com>
2025-05-04 12:11:23 +0000

    apiConfig file updated.

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,7 +1,3 @@
 // src/apiConfig.js
 
-const isReplit = window.location.hostname.includes("replit.dev");
-
-export const BACKEND_URL = isReplit
-  ? "https://fc511c5e-ad91-434b-b376-03734aabba98-00-15nynkqgkr4sv.sisko.replit.dev"
-  : "https://insights4096-backend.onrender.com";
+export const BACKEND_URL = "https://insights-4096-rbnbadri.replit.app";
