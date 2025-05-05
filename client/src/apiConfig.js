a226f7f0f20528687c53a88b9e409b843c3b6128
Badri Narayan <rbn.badri@gmail.com>
2025-05-05 06:57:57 +0000

    Restored default apiconfig

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,3 @@
 // src/apiConfig.js
 
-export const BACKEND_URL =
-  "https://3511f5bc-2f68-4d7c-9dbf-91f61b95d144-00-3oq2zcm1gff88.pike.replit.dev";
+export const BACKEND_URL = "https://api.insights4096.com";
